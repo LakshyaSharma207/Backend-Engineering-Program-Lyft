811a27192d9a24fb7c2a46b109274aabddb8bedd
LakshyaSharma207 <124886897+LakshyaSharma207@users.noreply.github.com>
2023-08-25 18:39:25 +0530

    Update battery_test.py

--- test/battery_test.py
+++ test/battery_test.py
@@ -7,13 +7,13 @@ import datetime
 
 class TestNubbinBattery(unittest.TestCase):
     def test_needs_service_true(self):
-        current_date = datetime.datetime(2022, 7, 31)
+        current_date = datetime.datetime(2023, 8, 18)
         last_service_date = datetime.datetime(2019,1,27)
         battery = NubbinBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
 
     def test_needs_service_false(self):
-        current_date = datetime.datetime(2022, 7, 31)
+        current_date = datetime.datetime(2023, 8, 18)
         last_service_date = datetime.datetime(2021,1,10)
         battery = NubbinBattery(current_date, last_service_date)
         self.assertFalse(battery.needs_service())
@@ -21,7 +21,7 @@ class TestNubbinBattery(unittest.TestCase):
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service_true(self):
-        current_date = datetime.datetime(2022, 7, 31)
+        current_date = datetime.datetime(2023, 8, 18)
         last_service_date = datetime.datetime(2017,1,25)
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
